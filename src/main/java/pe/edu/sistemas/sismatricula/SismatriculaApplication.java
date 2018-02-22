9c291080f96692c3dde15f81863dd1c24350801c
HenryJCM <henjacolonia17@gmail.com>
2018-02-22 18:19:32 -0500

    Proyecto iniciado - configuracion basica

---
+++ src/main/java/pe/edu/sistemas/sismatricula/SismatriculaApplication.java
@@ -0,0 +1,12 @@
+package pe.edu.sistemas.sismatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismatriculaApplication.class, args);
+	}
+}
