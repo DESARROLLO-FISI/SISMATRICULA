6e430d9f7e216797c4c3918a88ac2ad1db3300f5
HenryJCM <henjacolonia17@gmail.com>
2018-03-12 18:02:18 -0500

    webjars configurado

--- src/main/resources/static/js/logica.js
+++ src/main/resources/static/js/logica.js
@@ -1,30 +1,12 @@
-<<<<<<< HEAD
-/**
-class AlumnoModel {
-		
-		constructor(idAlumno,idPeriodo,alumnoCodigo,alumnoAppaterno,
-			alumnoApmaterno,alumnoNombre,alumnoEstado,alumnoIngreso) {
-		this.idAlumno = idAlumno;
-		this.idPeriodo = idPeriodo;
-		this.alumnoCodigo = alumnoCodigo;
-		this.alumnoAppaterno = alumnoAppaterno;
-		this.alumnoApmaterno = alumnoApmaterno;
-		this.alumnoNombre = alumnoNombre;
-		this.alumnoEstado = alumnoEstado;
-		this.alumnoIngreso = alumnoIngreso;
-	}
-		
-}**/
-
+//Variables Globales
 var json_ws;
 var string_ws;
 
 
-=======
-var json_ws;
-var string_ws;
-
->>>>>>> HenryJCM
+/** @input excel
+ * @process convertir excel a string
+ * @output String - json con datos Alumnos
+ */
 	function convertir (event) {
 	 
         loadBinaryFile(event,function(data){
@@ -32,11 +14,8 @@ var string_ws;
             var workbook = XLSX.read(data,{type:'binary'});
             
             var first_sheet_name = workbook.SheetNames[0];
-<<<<<<< HEAD
-=======
             
             document.getElementById("label-matricula").textContent = (document.getElementById("file-matricula").value).substring(12);
->>>>>>> HenryJCM
 
             var worksheet = workbook.Sheets[first_sheet_name]; 
             
@@ -57,28 +36,17 @@ var string_ws;
             };
             reader.readAsBinaryString(files[0]);
     }
-    
-<<<<<<< HEAD
-    
-=======
->>>>>>> HenryJCM
+   
+/** @input String Datos Alumnos
+ * @process Guardar Alumnos de manera masiva
+ * @output Mensaje de error o exito
+ */
     function enviarDataAlumnos(){
     	$.ajax({
              url: '/carga',
              type: 'POST', 
              contentType: "application/json; charset=utf-8",
              // el tipo de información que se espera de respuesta
-<<<<<<< HEAD
-             dataType: "json",
-             // la información a enviar
-             // (también es posible utilizar una cadena de datos)
-             data: string_ws,
-             timeout: 1000000,
-             // código a ejecutar si la petición es satisfactoria;
-             // la respuesta es pasada como argumento a la función
-             success: function(data) {            	             	 
-            	 console.log(data);
-=======
              dataType: "html",
              // la información a enviar
              // (también es posible utilizar una cadena de datos)
@@ -89,15 +57,10 @@ var string_ws;
             	 console.log("se entrego datos");
             	 $("#file-matricula").val("");
             	 $("#cargaExterna").html(data);
->>>>>>> HenryJCM
              },
              error : function(xhr, status) {
                  alert('Disculpe, existio un problema -- '+xhr+" -- "+status);
              },
-<<<<<<< HEAD
-             
-=======
->>>>>>> HenryJCM
            });
     }	
     	
